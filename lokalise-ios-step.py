030ee00d2c2f0dbba95068a4c9ed0ff98b3b1d33
jorthi <joris.thiery@smile.fr>
2024-11-18 11:53:44 +0100

    feat: get status of each translation

--- lokalise-ios-step.py
+++ lokalise-ios-step.py
@@ -47,7 +47,9 @@ print("\n json parsing === %s" % json['keys'], flush=True)
 print("\n json parsing === %s" % json['keys'][0], flush=True)
 
 for key in json['keys']:
-	 print("\n json parsing KEY === %s" % key, flush=True)
+	for translation in key['translations']:
+		status = translation['is_unverified']
+	 	print("\n status === %s" % status, flush=True)
 
 
 print("""\n\n
