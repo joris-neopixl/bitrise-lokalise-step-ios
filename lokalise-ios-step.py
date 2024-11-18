f4ef149768d706802cd16a0b6360ccda437558e4
jorthi <joris.thiery@smile.fr>
2024-11-18 12:48:55 +0100

    feat: manage paggination result

--- lokalise-ios-step.py
+++ lokalise-ios-step.py
@@ -31,24 +31,35 @@ file_path = "%s/%s" % (project_root_path, os.getenv('file_path'))
 
 # Get all translations from Lokalise 
 import requests
-url = "https://api.lokalise.com/api2/projects/%s/keys?include_comments=0&include_translations=1&pagination=offset&limit=500" % lokalise_project_id
+
 headers = {
     "accept": "application/json",
     "X-Api-Token": "%s" % lokalise_token
 }
-response = requests.get(url, headers=headers)
+
+url_project = "https://api.lokalise.com/api2/projects/%s" % lokalise_project_id
+response_project = requests.get(url_project, headers=headers)
+
+url_translations = "https://api.lokalise.com/api2/projects/%s/keys?include_comments=0&include_translations=1&pagination=offset&limit=500&page=1" % lokalise_project_id
+response_translation = requests.get(url_translations, headers=headers)
 
 
 # Read & Parse JSON
-json = response.json()
+json_project = response_project.json()
+json_translation = response_translation.json()
 
-print("\n Verifing translations status...", flush=True)
+print("\n Verifing the setting of the project...", flush=True)
+
+keys_total = json_project['statistics']['keys_total']
+print("\n      -> There is %s keys in the project\n", % keys_total ) flush=True)
+
+print("\n Verifing %s translations status...", % len(json['keys']) flush=True)
 
 for key in json['keys']:
     for translation in key['translations']:
 	    status = translation['is_unverified']
 	    if status == True:
-	        print("\n ! ! ! ERROR ! ! ! \n\n: SOME KEY(S) (%s) ARE NOT VERIFIED, PLEASE VERIFY ALL KEYS BEFORE CREATING A RELASE \n \n ! ! ! ERROR ! ! !" % key['key_name']['ios'], flush=True)
+	        print("\n\n\n ! ! ! ERROR ! ! ! \n\n\n\n\n: SOME KEY(S) (%s) ARE NOT VERIFIED, PLEASE VERIFY ALL KEYS BEFORE CREATING A RELASE \n\n\n\n\n ! ! ! ERROR ! ! !\n\n\n" % key['key_name']['ios'], flush=True)
 	        os._exit(12)
 
 print("\n All keys are 'verified', let's continue and update the project with fresh translations :D", flush=True)
